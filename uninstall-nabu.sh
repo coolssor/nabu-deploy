d46499d90fca059906ae97a0c2f33f3f014b9f24
Ross Coull <coolssor@oe.tc>
2025-07-31 12:07:50 +0100

    Update uninstall-nabu.sh
    Remove ollama

--- uninstall-nabu.sh
+++ uninstall-nabu.sh
@@ -25,7 +25,7 @@ function error_exit() {
 }
 
 # Stop and remove known containers
-containers=(open-webui mcpo ollama tts)
+containers=(open-webui mcpo tts)
 
 echo -e "${BLUE}Stopping and removing Nabu containers...${RESET}"
 for cname in "${containers[@]}"; do
